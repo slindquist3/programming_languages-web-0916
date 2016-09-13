0a7b36e749c47db0845a03a8d2d96ff13c4d9a64
slindquist3 <slindquist3@gmail.com>
2016-09-13 19:47:13 -0400

    Modifed hashes of programming languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,43 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  new_hash[:ruby] = {type: "interpreted", style: [:oo]}
+  new_hash[:javascript] = {type: "interpreted", style: [:oo, :functional]}
+  new_hash[:python] = {type: "interpreted", style: [:oo]}
+  new_hash[:java] = {type: "compiled", style: [:oo]}
+  new_hash[:clojure] = {type: "compiled", style: [:functional]}
+  new_hash[:erlang] = {type: "compiled", style: [:functional]}
+  new_hash[:scala] = {type: "compiled", style: [:functional]}
+  new_hash
 end
